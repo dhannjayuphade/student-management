f8b248c12848ad4653b63631d51833d5d38e2fb9
Dhannjay uphade <dhannjayuphade5@gmail.com>
2026-08-13 20:11:29 +0530

    Ensure newline at end of firebase.js
    Fix formatting by ensuring proper newline at end of file.

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js"; 
 import {
   getAuth,
   onAuthStateChanged,
