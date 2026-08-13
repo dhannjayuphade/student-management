b6d4b248a9dd545b00b7aadd3724f8abbc65a4e3
Dhannjay uphade <dhannjayuphade5@gmail.com>
2026-08-13 19:33:59 +0530

    Initialize Firebase and export auth/Firestore functions
    This file initializes Firebase with the given configuration and exports the necessary authentication and Firestore functions.

---
+++ firebase.js
@@ -0,0 +1,65 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  updateProfile
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  collection,
+  query,
+  where,
+  orderBy,
+  onSnapshot,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTIKG1yfUqXoDY7FLDlAJxXvOkaO6-44U",
+  authDomain: "student-study-manager.firebaseapp.com",
+  projectId: "student-study-manager",
+  storageBucket: "student-study-manager.firebasestorage.app",
+  messagingSenderId: "124945078913",
+  appId: "1:124945078913:web:1daf63383001944e13cb80",
+  measurementId: "G-LSE43DVZNV"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export {
+  app,
+  auth,
+  db,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  updateProfile,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  collection,
+  query,
+  where,
+  orderBy,
+  onSnapshot,
+  serverTimestamp
+};
